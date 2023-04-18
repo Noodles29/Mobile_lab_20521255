1c9458178858b979296b16bbbd6450c92a126208
unknown <phamduyenn2906@gmail.com>
2023-04-18 20:55:24 +0700

    first update lab2

---
+++ app/src/main/java/com/example/mobile_lab/Employee.java
@@ -0,0 +1,57 @@
+package com.example.mobile_lab;
+
+
+import java.text.DecimalFormat;
+
+public class Employee {
+
+    protected String Name;
+    protected double Salary;
+    protected double NetSalary;
+
+
+    public Employee(String a, double b) {
+        this.Name = a;
+        this.Salary = b;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(){
+        this.Name = Name;
+    }
+
+    public double getSalary(){
+        return Salary;
+    }
+
+    public void setSalary(){
+        this.Salary = Salary;
+    }
+
+    public double getNetSalary(){
+        return NetSalary;
+    }
+
+    public void setNetSalary(){
+        this.NetSalary = NetSalary;
+    }
+
+    public String calculationNetSalary(){
+        double a, tax;
+        DecimalFormat df = new DecimalFormat("#,###.#");
+
+        a = this.Salary - this.Salary * 0.105;
+        tax = (this.Salary - 11000000) * 0.05;
+
+        if (a <= 11000000){
+            NetSalary = a;
+        } else {
+            NetSalary = a;
+        }
+
+        return df.format(NetSalary);
+    }
+}
